127e3e78c894303ef469a986e8649f0507712f60
Laguna1 <lagunazoomarket@gmail.com>
2021-06-09 23:25:07 +0300

    Change password to encrypted

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_193050) do
+ActiveRecord::Schema.define(version: 2021_06_09_201635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
+    t.string "encrypted_password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
     username { Faker::Internet.username(specifier: 5..20) }
-    pasword_digest { 'password' }
+    password { 'password' }
   end
 end
